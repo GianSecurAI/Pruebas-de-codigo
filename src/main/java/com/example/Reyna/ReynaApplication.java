1801ddc44bf006d37f379ee93c9d871645846784
Gianmarco <gianmarcomenacho2@gmail.com>
2025-05-14 16:52:40 -0500

    Añadiendo la parte del Backend

---
+++ src/main/java/com/example/Reyna/ReynaApplication.java
@@ -0,0 +1,13 @@
+package com.example.Reyna;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReynaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReynaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Reyna/ReynaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Reyna;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReynaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
